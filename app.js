a934af4475f330700f65152361a194aee13d758b
Aires <aires@localhost.localdomain>
2019-09-19 14:26:14 -0300

    Testing endpoints on express

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 var app = express();
 
 app.get('/', function(req, res) {
     res.send(JSON.stringify({ Hello: "World" }));
 });
+
+app.get('/about', function(req, res) {
+    res.send(JSON.stringify({ About: "me" }));
+});
+
 
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
