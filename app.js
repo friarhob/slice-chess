c7c1fa4af3e3452669043d152be28a0332c18079
Aires <aires@localhost.localdomain>
2019-09-25 14:55:11 -0300

    Adding swagger on /api-docs endpoint, documenting basic swagger for /engines and /engines/{id}

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-var express = require("express");
-var port = process.env.PORT || 8000;
+const express = require("express");
+const port = process.env.PORT || 8000;
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+  swaggerDefinition: {
+    info: {
+        title: "SLICE - Set of Learning and Immutable Chess Engines",
+        version: "1.0.0",
+        host: "https://slice-chess.herokuapp.com/",
+        "base-path": "/",
+        openapi: "3.0.0"
+   },
+  },
+  apis: ['app.js'],
+};
+const specs = swaggerJsdoc(options);
 
 var engines = {
     randony: new (require('./engines/randony.js').Randony)()
@@ -7,29 +22,47 @@ var engines = {
 
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send(JSON.stringify({ Hello: "World" }));
-});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get('/about', function(req, res) {
-    res.send(JSON.stringify({ About: "me" }));
+    res.send(JSON.stringify({ Author: "Friar Hob" }));
 });
 
+/**
+ * @swagger
+ * /engines:
+ *      get:
+ *          description: This should return a list with all engines available
+ *      
+ */
+app.get('/engines', function(req, res) {
+    var enginesDesc = [];
+    for(var item in engines) {
+//        console.log(item.toString());
+        enginesDesc.push(item.toString());
+    }  
+    res.send(JSON.stringify({ engineIDs: enginesDesc }));
+});
+
+/**
+ * @swagger
+ * /engines/{id}:
+ *      get:
+ *          description: next move from engine represented by ID
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: the ID of the engine
+ *          - in: query
+ *            name: fen
+ *            description: FEN representation of current board
+ */
 app.get('/engines/:id', function(req, res) {
     var move;
     move = engines[req.params.id].nextMove(req.query.fen);
     res.send(JSON.stringify({nextMove: move}));
 });
-
-app.get('/engines', function(req, res) {
-    var enginesDesc = [];
-    engines.forEach(function (item, index) {
-//        console.log(item.toString());
-        enginesDesc.push(item.toString());
-    });  
-    res.send(JSON.stringify({ engineIDs: enginesDesc }));
-});
-
 
 app.listen(port, function () {
  console.log(`app listening on port 8000!`);
